f05d6907741ac35d7f19d557a3b8666d582f460f
unknown <yogifst@gmail.com>
2025-07-10 12:20:25 +0530

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // ✅ This is where Tailwind gets pulled in
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
