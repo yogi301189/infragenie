7d3882aeb2bb99d8b60ff455410a25dce5799bca
unknown <yogifst@gmail.com>
2025-07-10 13:06:21 +0530

    idea1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import PromptForm from "./PromptForm";
+import Docs from "./pages/Docs"; // Make sure Docs.jsx is inside /src/pages
 
 function App() {
   return (
-    <>
-      <LandingPage />
-      <section id="prompt" className="bg-slate-900 py-24 px-4">
-        <PromptForm />
-      </section>
-    </>
+    <Router>
+      <Routes>
+        {/* Home Page Route */}
+        <Route
+          path="/"
+          element={
+            <>
+              <LandingPage />
+              <section id="prompt" className="bg-slate-900 py-24 px-4">
+                <PromptForm />
+              </section>
+            </>
+          }
+        />
+
+        {/* Docs Route */}
+        <Route path="/docs" element={<Docs />} />
+      </Routes>
+    </Router>
   );
 }
 
