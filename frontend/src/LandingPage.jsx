e2727beb54472a8055f07e7205b09739f8c0eb14
unknown <yogifst@gmail.com>
2025-07-12 22:22:02 +0530

    nest

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React from "react";
 import { Button } from "./components/ui/button";
 import { Card, CardContent } from "./components/ui/card";
 import { Copy } from "lucide-react";
